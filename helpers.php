7795c1270781d27722f937d5c7b9ed12056ec22d
Quynh Xuan Nguyen <seriquynh@gmail.com>
2021-12-27 11:04:51 +0700

    wip

---
+++ helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+use PhpOption\Option;
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = $_SERVER[$key] ?? $_ENV[$key];
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+            return $matches[2];
+        }
+
+        return $value ?? $default;
+    }
+}
